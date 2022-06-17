25efd79a54776895d1c3aa3f4eb7a98f30de9734
PHJoemar <joemar.vargas24@gmail.com>
2022-06-18 03:57:30 +0800

    Removed unnecessary comments

--- js/index.js
+++ js/index.js
@@ -1,28 +1,24 @@
-// Create a function named computerPlay that returns Rock, Paper or Scissors
 function computerPlay() {
-    // Create a variable that gives random number between 1 and 3
+    // A variable that gives random number between 1 and 3
     let computer = (Math.floor(Math.random() * 3) + 1);
 
-    // If the variable returns 1 THEN return Rock
     if (computer == 1) {
         return "Rock";
     }
-    // If the variable returns 2 THEN return Paper
+    
     if (computer == 2) {
         return "Paper";
     }
-    // If the variable returns 3 THEN return Scissors
+    
     if (computer == 3) {
         return "Scissors";
     }
 }
 
-// Create a function named playRound that plays a single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
     let player = playerSelection.trim().toLowerCase();
     let computer = computerSelection.toLowerCase();
-
-    // Create an IF statements if player selected Rock
+    
     if (player == "rock" && computer == "rock") {
         return "Tie!"
     }
@@ -32,8 +28,7 @@ function playRound(playerSelection, computerSelection) {
     if (player == "rock" && computer == "scissors") {
         return "You Win! Rock beats Scissors!"
     }
-
-    // Create an IF statements if player selected Paper
+    
     if (player == "paper" && computer == "rock") {
         return "You Win! Paper beats Rock!"
     }
@@ -43,8 +38,7 @@ function playRound(playerSelection, computerSelection) {
     if (player == "paper" && computer == "scissors") {
         return "You Lose! Scissors beats Paper!"
     }
-
-    // Create an IF statements if player selected Scissors
+    
     if (player == "scissors" && computer == "rock") {
         return "You Lose! Rock beats Scissors!"
     }
@@ -56,19 +50,12 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// Testing the functions above
-// const playerSelection = "rock";
-// const computerSelection = computerPlay();
-// console.log(playRound(playerSelection, computerSelection));
-
-// Create a function named game that computes scores and loops five round game
 function game() {
     let yourScore = 0;
     let tie = 0;
     let enemyScore = 0;
-
+    
     for (let i = 0; i < 5; i++) {
-        // your code here!
         const playerSelection = prompt(`Round ${i+1}: [Rock] [Paper] [Scissors] `, "");
 
         if (playerSelection == undefined || playerSelection == "") {
@@ -79,27 +66,26 @@ function game() {
             const computerSelection = computerPlay();
             let results = playRound(playerSelection, computerSelection);
             
-            if (results.toLowerCase().includes("win")) {    // IF user won THEN add 1 in yourScore variable
+            if (results.toLowerCase().includes("win")) {
                 alert(playRound(playerSelection, computerSelection));
                 yourScore += 1;
-            } else if (results.toLowerCase().includes("tie")) {    // IF round game is tie THEN 
+            } else if (results.toLowerCase().includes("tie")) {
                 alert(playRound(playerSelection, computerSelection));
                 tie += 1;
             } else {
                 alert(playRound(playerSelection, computerSelection));
                 enemyScore += 1;
             }
-            // Show result per round
+
+            // Shows the result per round
             console.log(playRound(playerSelection, computerSelection));
         } else {
             return alert("Invalid input!\nPlease reload the page then select one. [Rock] [Paper] [Scissors]");
         }
     }
 
-    // Show the results
     console.log(`Your score is ${yourScore} out of 5 and ${tie} tie(s).`);
 
-    // Check if who is the winner
     if (yourScore === enemyScore) {
         alert(`It's a draw. Your score is ${yourScore} out of 5 and ${tie} tie(s).`);
         console.log("Draw");
